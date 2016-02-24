96a1e04e86a67854f2fb124904c6a7d14a4d2842
Maksadbek <a.maksadbek@gmail.com>
2016-02-24 06:46:19 +0500

    http hander to send items

--- xmlroom.go
+++ xmlroom.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/Maksadbek/xmlroom/datastore"
 	"github.com/Maksadbek/xmlroom/models"
 )
 
-var dbFilePath = flag.String("db", "./db", "database file path")
+var (
+	dbFilePath = flag.String("db", "./db", "database file path")
+	port       = flag.String("port", ":8080", "server port")
+)
 
 func main() {
 	flag.Parse()
@@ -39,4 +46,27 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	http.HandleFunc("/", IndexHandler)
+	http.ListenAndServe(*port, nil)
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	m := r.URL.Query().Get("m")
+	log.Println(m)
+	memberID, err := strconv.Atoi(m)
+	if err != nil {
+		http.Error(w, "please provide member id", http.StatusBadRequest)
+	}
+	house := models.Housing{}
+	items, err := datastore.ReadItemsByMember(memberID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	house.Member.EstateAgentID = memberID
+	house.Member.Items = items
+	itemsJ, err := json.Marshal(house)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, string(itemsJ))
 }
